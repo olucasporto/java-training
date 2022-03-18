bdc157b613fbfa39847333dda7dac40c23e25c52
Lucas Porto <olucasporto@gmail.com>
2022-03-18 07:59:10 -0400

    adding more projects

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá novo teste");	
+		
+		int idade = 37;
+		
+		System.out.println("A idade de Jorginho é " + idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println("A idade de Fernandinho é " + idade);
+		
+		idade = 7 * (5 + 2);
+		
+		System.out.println("A idade de Pedrinho é " + idade);
+		
+		
+	}
+	
+}
