ae3fc8883115953a1b2d2d8c4dca23b37a463be9
Lucas Porto <olucasporto@gmail.com>
2022-03-21 01:55:48 -0300

    Terminando primeira etapa do curso de Java

---
+++ sintaxe-variaveis-e-fluxo/src/TestaLacos3.java
@@ -0,0 +1,13 @@
+
+public class TestaLacos3 {
+
+	public static void main(String[] args) {
+
+		for (int contador = 3; contador <= 100; contador += 3) {
+
+			System.out.println(contador);
+
+		}
+
+	}
+}
