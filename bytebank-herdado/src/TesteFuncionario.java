e9129310522777c05af9fd97eece6f3ff7979608
Lucas Porto <olucasporto@gmail.com>
2022-03-25 00:00:47 -0300

    Terminando aula de Super e reescrita de metodos

--- bytebank-herdado/src/TesteFuncionario.java
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -2,14 +2,14 @@
 public class TesteFuncionario {
 
 	public static void main(String[] args) {
-		
+
 		Funcionario lucao = new Funcionario();
 		lucao.setNome("Lucas Porto");
 		lucao.setCpf("000333111-22");
 		lucao.setSalario(90000);
-		
+
 		System.out.println(lucao.getNome());
 		System.out.println(lucao.getBonificacao());
 	}
-	
+
 }
--- bytebank-herdado/src/TesteGerente.java
+++ bytebank-herdado/src/TesteGerente.java
@@ -6,17 +6,18 @@ public class TesteGerente {
 		lucao.setNome("Lucas Porto");
 		lucao.setCpf("03334516");
 		lucao.setSalario(3000);
-		
+
 		System.out.println(lucao.getNome());
 		System.out.println(lucao.getCpf());
 		System.out.println(lucao.getSalario());
-		
+
 		lucao.setSenha(2222);
-		
+
 		boolean autenticou = lucao.autentica(2222);
-		
+
 		System.out.println(autenticou);
 
+		System.out.println(lucao.getBonificacao());
 	}
 
 }
