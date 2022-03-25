6f4cf934ebf9c2c5d86843821e2aea7f5c1c10ae
Lucas Porto <olucasporto@gmail.com>
2022-03-24 23:19:52 -0300

    Terminando aula de Introducao a Heranca

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario lucao = new Funcionario();
+		lucao.setNome("Lucas Porto");
+		lucao.setCpf("000333111-22");
+		lucao.setSalario(90000);
+		
+		System.out.println(lucao.getNome());
+		System.out.println(lucao.getBonificacao());
+	}
+	
+}
---
+++ bytebank-herdado/src/TesteGerente.java
@@ -0,0 +1,22 @@
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+		Gerente lucao = new Gerente();
+		lucao.setNome("Lucas Porto");
+		lucao.setCpf("03334516");
+		lucao.setSalario(3000);
+		
+		System.out.println(lucao.getNome());
+		System.out.println(lucao.getCpf());
+		System.out.println(lucao.getSalario());
+		
+		lucao.setSenha(2222);
+		
+		boolean autenticou = lucao.autentica(2222);
+		
+		System.out.println(autenticou);
+
+	}
+
+}
